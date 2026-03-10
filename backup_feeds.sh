7b0e2ad9a70ff14f7c7572281458f2bd38f26dba
Alexandr Savca <alexandr.savca89@gmail.com>
2026-03-10 11:03:33 +0200

    remove obsoleted scripts

--- backup_feeds.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Backup snownews' local feeds.
-CURDIR=$PWD
-(cd ~/; tar -czf $CURDIR/backup.tar.gz .local/share/snownews/)
--- check_urls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Check urls.opml file for dead links.
-
-grep -Eiho "https?://[^\"\\'> ]+" urls.opml | xargs -r -P10 -I{} \
-	curl -I -o /dev/null -sw "[%{http_code}] %{url}\n" '{}'  |
-	grep -v '^\[200\]' | sort -u
-
-# End of file.
